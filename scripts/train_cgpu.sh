c1620a6c79bef7768937889a13c9c67dc3ab50b1
Steve Farrell <sparticlesteve@gmail.com>
2019-06-11 22:38:19 -0700

    added full checkpoint, resume, and history logging

--- scripts/train_cgpu.sh
+++ scripts/train_cgpu.sh
@@ -4,9 +4,10 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mem=30GB
 #SBATCH -t 8:00:00
+#SBATCH -d singleton
 #SBATCH -J train-cgpu
 #SBATCH -o logs/%x-%j.out
 
 mkdir -p logs
 . scripts/setup_cgpu.sh
-srun python train.py
+srun python train.py $@
--- train.py
+++ train.py
@@ -8,9 +8,10 @@ import argparse
 import logging
 
 # External imports
-import numpy as np
-import tensorflow as tf
 import yaml
+import numpy as np
+import pandas as pd
+import tensorflow as tf
 
 # Local imports
 from data import get_datasets
@@ -28,7 +29,8 @@ def parse_args():
     add_arg('config', nargs='?', default='configs/cosmo.yaml')
     add_arg('-d', '--distributed', action='store_true')
     add_arg('-v', '--verbose', action='store_true')
-    add_arg('--interactive', action='store_true')
+    add_arg('--resume', action='store_true',
+            help='Resume from last checkpoint')
     return parser.parse_args()
 
 def config_logging(verbose):
@@ -40,6 +42,24 @@ def load_config(config_file):
     """Reads the YAML config file and returns a config dictionary"""
     with open(config_file) as f:
         return yaml.load(f, Loader=yaml.FullLoader)
+
+def load_history(output_dir):
+    return pd.read_csv(os.path.join(output_dir, 'history.csv'))
+
+def print_training_summary(output_dir):
+    history = load_history(output_dir)
+    best = history.val_loss.idxmin()
+    logging.info('Best result:')
+    for key in history.keys():
+        logging.info('  %s: %g', key, history[key].loc[best])
+
+def reload_last_checkpoint(checkpoint_format, n_epochs):
+    for epoch in range(n_epochs, 0, -1):
+        checkpoint = checkpoint_format.format(epoch=epoch)
+        if os.path.exists(checkpoint):
+            model = tf.keras.models.load_model(checkpoint)
+            return epoch, model
+    raise Exception('Unable to find a checkpoint file at %s' % checkpoint_format)
 
 def main():
     """Main function"""
@@ -59,52 +79,52 @@ def main():
     data_config = config['data']
     train_data, valid_data = get_datasets(**data_config)
 
-    # Build the model
-    model = get_model(**config['model'])
+    # Construct or reload the model
+    initial_epoch = 0
+    checkpoint_format = os.path.join(output_dir, 'checkpoint-{epoch:03d}.h5')
+    if args.resume:
+        # Reload model from last checkpoint
+        initial_epoch, model = reload_last_checkpoint(
+            checkpoint_format, data_config['n_epochs'])
+    else:
+        # Build a new model
+        model = get_model(**config['model'])
+        # Configure the optimizer
+        opt = get_optimizer(n_ranks=n_ranks,
+                            distributed=args.distributed,
+                            **config['optimizer'])
+        # Compile the model
+        train_config = config['train']
+        model.compile(optimizer=opt, loss=train_config['loss'],
+                      metrics=train_config['metrics'])
 
-    # Configure the optimizer
-    opt = get_optimizer(n_ranks=n_ranks,
-                        distributed=args.distributed,
-                        **config['optimizer'])
-
-    # Compile the model
-    train_config = config['train']
-    model.compile(optimizer=opt, loss=train_config['loss'],
-                  metrics=train_config['metrics'])
     if rank == 0:
         model.summary()
 
     # Prepare the callbacks
     callbacks = []
-
     if rank == 0:
-        callbacks.append(tf.keras.callbacks.ModelCheckpoint(
-            os.path.join(output_dir, 'checkpoint-{epoch:03d}.ckpt'),
-            save_weights_only=True,
-        ))
+        # Checkpointing
+        callbacks.append(tf.keras.callbacks.ModelCheckpoint(checkpoint_format))
+        # CSV logging
+        callbacks.append(tf.keras.callbacks.CSVLogger(
+            os.path.join(output_dir, 'history.csv'), append=args.resume))
 
     # Train the model
     n_train = data_config['n_train_files'] * data_config['samples_per_file']
     n_valid = data_config['n_valid_files'] * data_config['samples_per_file']
-    history = model.fit(train_data,
-                        steps_per_epoch=n_train//data_config['batch_size'],
-                        epochs=data_config['n_epochs'],
-                        validation_data=valid_data,
-                        validation_steps=n_valid//data_config['batch_size'],
-                        callbacks=callbacks,
-                        verbose=2)
+    model.fit(train_data,
+              steps_per_epoch=n_train//data_config['batch_size'],
+              epochs=data_config['n_epochs'],
+              validation_data=valid_data,
+              validation_steps=n_valid//data_config['batch_size'],
+              callbacks=callbacks,
+              initial_epoch=initial_epoch,
+              verbose=(1 if args.verbose else 2))
 
-    # Save the training history
+    # Print training summary
     if rank == 0:
-        # Print best metrics
-        best_epoch = np.argmin(history.history['val_loss'])
-        logging.info('Best epoch: %i', best_epoch)
-        for key, val in history.history.items():
-            logging.info('  %s: %.4f', key, val[best_epoch])
-
-        # Save the history to file
-        np.savez(os.path.join(output_dir, 'history'),
-                 **history.history)
+        print_training_summary(output_dir)
 
     if rank == 0:
         logging.info('All done!')
