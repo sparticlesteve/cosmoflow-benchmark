c9c315b0e5bd85f28e40400e9ecf751b58a96963
Steve Farrell <sparticlesteve@gmail.com>
2019-06-18 05:14:50 -0700

    adding omp settings to setup_cori script

--- scripts/setup_cori.sh
+++ scripts/setup_cori.sh
@@ -1,2 +1,5 @@
 # Source this script to setup the runtime environment on cori
+export OMP_NUM_THREADS=32
+export KMP_BLOCKTIME=1
+export KMP_AFFINITY="granularity=fine,compact,1,0"
 module load tensorflow/intel-1.13.1-py36
