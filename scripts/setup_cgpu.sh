20b7a5f21214fb0bc10b0583e9549e46a24c045a
Steve Farrell <sparticlesteve@gmail.com>
2019-06-19 04:35:46 -0700

    update cori gpu batch script for multi-gpu training

--- scripts/setup_cgpu.sh
+++ scripts/setup_cgpu.sh
@@ -1,4 +1,7 @@
 # Source this script to setup the runtime environment on cori
 module load cuda
+module load nccl
 module load tensorflow/gpu-1.13.1-py36
+module load openmpi/3.1.0-ucx
+
 export PATH=$PYTHONUSERBASE/bin:$PATH
