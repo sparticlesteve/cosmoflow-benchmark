486e7273e485f77cd939987f65ee0eb5b818af9e
Steve Farrell <sparticlesteve@gmail.com>
2019-06-25 18:04:55 -0700

    cori benchmarking config

---
+++ scripts/train_cori.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -C haswell
+#SBATCH -q debug
+#SBATCH -t 30
+#SBATCH -J train-cori
+#SBATCH -d singleton
+#SBATCH -o logs/%x-%j.out
+
+mkdir -p logs
+. scripts/setup_cori.sh
+
+srun -l -u python train.py -d $@
