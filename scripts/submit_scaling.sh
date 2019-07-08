9ca5fddc0bdd2389253ab34648cef4f0a92d46c2
Steve Farrell <sparticlesteve@gmail.com>
2019-07-08 14:37:55 -0700

    adding scaling submission script

---
+++ scripts/submit_scaling.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -ex
+
+# Weak scaling dataset size
+sbatch -N 1 scripts/train_cori.sh --data-config "{n_train_files: 4}"
+sbatch -N 2 scripts/train_cori.sh --data-config "{n_train_files: 8}"
+sbatch -N 4 scripts/train_cori.sh --data-config "{n_train_files: 16}"
+sbatch -N 8 scripts/train_cori.sh --data-config "{n_train_files: 32}"
+sbatch -N 16 scripts/train_cori.sh --data-config "{n_train_files: 64}"
+sbatch -N 32 scripts/train_cori.sh --data-config "{n_train_files: 128}"
+sbatch -N 64 scripts/train_cori.sh --data-config "{n_train_files: 256}"
+
+#sbatch -N 1 scripts/train_cori.sh
+#sbatch -N 2 scripts/train_cori.sh
+#sbatch -N 4 scripts/train_cori.sh
+#sbatch -N 8 scripts/train_cori.sh
+#sbatch -N 16 scripts/train_cori.sh
+#sbatch -N 32 scripts/train_cori.sh
+#sbatch -N 64 scripts/train_cori.sh
