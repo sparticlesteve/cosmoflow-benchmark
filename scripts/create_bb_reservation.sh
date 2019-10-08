57310b007ef4ee270189ffac17c2ba8615cfa413
Steve Farrell <sparticlesteve@gmail.com>
2019-10-07 21:54:44 -0700

    added docs link and printout to BB script

--- scripts/create_bb_reservation.sh
+++ scripts/create_bb_reservation.sh
@@ -4,3 +4,7 @@
 #SBATCH -C haswell
 #SBATCH -t 5
 #BB create_persistent name=cosmobb capacity=5TB access_mode=striped type=scratch 
+
+# Burst buffer docs:
+# https://docs.nersc.gov/jobs/examples/#burst-buffer
+echo "Creating persistent BB reservation cosmobb"
