34bfb1e2c00a2a757876c7da5b5c804f8a18eb67
Steve Farrell <sparticlesteve@gmail.com>
2019-10-20 17:17:17 -0700

    update cori setup scripts

--- scripts/setup_cori.sh
+++ scripts/setup_cori.sh
@@ -2,4 +2,6 @@
 export OMP_NUM_THREADS=32
 export KMP_BLOCKTIME=1
 export KMP_AFFINITY="granularity=fine,compact,1,0"
+export HDF5_USE_FILE_LOCKING=FALSE
 module load tensorflow/intel-1.13.1-py36
+module list
