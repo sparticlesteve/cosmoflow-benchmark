0b917c08f9749ce490ef96c7f2a6bfc1cdbfeac2
Steve Farrell <sparticlesteve@gmail.com>
2020-02-05 14:25:03 -0800

    moved cori scripts to TF 1.15

--- scripts/setup_cori.sh
+++ scripts/setup_cori.sh
@@ -3,5 +3,5 @@ export OMP_NUM_THREADS=32
 export KMP_BLOCKTIME=1
 export KMP_AFFINITY="granularity=fine,compact,1,0"
 export HDF5_USE_FILE_LOCKING=FALSE
-module load tensorflow/intel-1.13.1-py36
+module load tensorflow/intel-1.15.0-py37
 module list
