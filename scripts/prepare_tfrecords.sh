18dfac480fb3623231dfc3be06f9e458ad8405bb
Steve Farrell <sparticlesteve@gmail.com>
2020-02-20 14:02:01 -0800

    update prepare prod script

--- scripts/prepare_tfrecords.sh
+++ scripts/prepare_tfrecords.sh
@@ -5,14 +5,15 @@
 #SBATCH -t 30
 #SBATCH -o logs/%x-%j.out
 
+set -e
 mkdir -p logs
 . scripts/setup_cori.sh
 
 # Loop over tasks, 1 per node
 for (( i=0; i<$SLURM_JOB_NUM_NODES; i++ )); do
     echo "Launching task $i"
-    srun -N 1 python prepare.py \
-        --max-files 4096 \
+    srun -N 1 -c 64 python prepare.py \
+        --max-files 8192 \
         --n-workers 32 --task $i --n-tasks $SLURM_JOB_NUM_NODES &
 done
 wait
