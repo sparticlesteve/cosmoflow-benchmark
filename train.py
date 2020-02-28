45e4207efc55c52bf5693087d22e94b8855d9453
Steve Farrell <sparticlesteve@gmail.com>
2020-02-27 19:03:43 -0800

    Added command line arg --apply-log

--- train.py
+++ train.py
@@ -46,6 +46,7 @@ def parse_args():
     add_arg('--n-valid', type=int, help='Override number of validation samples')
     add_arg('--batch-size', type=int, help='Override the batch size')
     add_arg('--n-epochs', type=int, help='Override number of epochs')
+    add_arg('--apply-log', type=int, choices=[0, 1], help='Apply log transform to data')
 
     # Hyperparameter settings
     add_arg('--conv-size', type=int, help='CNN size parameter')
@@ -101,6 +102,8 @@ def load_config(args):
         config['data']['batch_size'] = args.batch_size
     if args.n_epochs is not None:
         config['data']['n_epochs'] = args.n_epochs
+    if args.apply_log is not None:
+        config['data']['apply_log'] = bool(args.apply_log)
 
     # Hyperparameters
     if args.conv_size is not None:
