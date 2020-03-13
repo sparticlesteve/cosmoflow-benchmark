2d5983bd01d4e0f513e84c3728693ee00f758ae4
Steve Farrell <sparticlesteve@gmail.com>
2020-03-13 09:11:17 -0700

    update prepare_tfrecords for new prod

--- scripts/prepare_tfrecords.sh
+++ scripts/prepare_tfrecords.sh
@@ -5,16 +5,15 @@
 #SBATCH -t 30
 #SBATCH -o logs/%x-%j.out
 
-maxFiles=32 #8192
+maxFiles=8192
 
 set -e
-mkdir -p logs
 . scripts/setup_cori.sh
 
 # Loop over tasks, 1 per node
 for (( i=0; i<$SLURM_JOB_NUM_NODES; i++ )); do
     echo "Launching task $i"
-    srun -N 1 -c 64 python prepare.py \
+    srun -N 1 -n 1 -c 64 python prepare.py \
         --max-files $maxFiles \
         --n-workers 32 --task $i --n-tasks $SLURM_JOB_NUM_NODES &
 done
