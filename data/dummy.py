3684c0c0f129614900d6cffeb25a5e0cd2f40b0a
Steve Farrell <sparticlesteve@gmail.com>
2020-07-06 22:05:18 -0700

    bugfix for dummy data

--- data/dummy.py
+++ data/dummy.py
@@ -17,8 +17,7 @@ def construct_dataset(sample_shape, target_shape,
     return data.repeat().batch(batch_size).prefetch(4)
 
 def get_datasets(sample_shape, target_shape, batch_size,
-                 n_train, n_valid, n_epochs=None, shard=False,
-                 rank=0, n_ranks=1):
+                 n_train, n_valid, dist, n_epochs=None, shard=False):
     train_dataset = construct_dataset(sample_shape, target_shape, batch_size=batch_size)
     valid_dataset = None
     if n_valid > 0:
@@ -26,8 +25,8 @@ def get_datasets(sample_shape, target_shape, batch_size,
     n_train_steps = n_train  // batch_size
     n_valid_steps = n_valid  // batch_size
     if shard:
-        n_train_steps = n_train_steps // n_ranks
-        n_valid_steps = n_valid_steps // n_ranks
+        n_train_steps = n_train_steps // dist.size
+        n_valid_steps = n_valid_steps // dist.size
 
     return dict(train_dataset=train_dataset, valid_dataset=valid_dataset,
                 n_train=n_train, n_valid=n_valid, n_train_steps=n_train_steps,
